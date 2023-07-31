368670d3dc174aafce23d03b1d992fafec128caa
abhyudaya29 <abhyudayadev123@gmail.com>
2023-08-01 01:54:16 +0530

    Data Transformation.

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -16,6 +16,47 @@ from xgboost import XGBRegressor
 from src.exception import CustomException
 from src.logger import logging
 
-from src.utils import save_object
+from src.utils import save_object,evaluate_models
 
 @dataclass
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join("artifacts","model.pkl")
+
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+    def initiate_model_trainer(self,train_array,test_array,preprocessor_path):
+        try:
+            logging.info("splitting train and test input data")
+            X_train,y_train,X_test,y_test=(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+            models={
+                "Random Forest":RandomForestRegressor(),
+                "Decision Tree":DecisionTreeRegressor(),
+                "Gradient Boostng":GradientBoostingRegressor(),
+                "Linear Regressor":LinearRegression(),
+                "K-Neighbours Classifier":KNeighborsRegressor(),
+                "XGBclassifer":XGBRegressor(),
+                "CatBoosting":CatBoostRegressor(verbose=False),
+                "AdaBoost Classifier":AdaBoostRegressor(),
+            }
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,x_test=X_test,y_test=y_test,models=models)
+
+            ## To get best model score from dict
+            best_model_score=max(sorted(model_report.values()))
+            ## TO get best model name from dict
+            best_model_name=list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model=models[best_model_name]
+
+            if best_model_score<0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best found model on both traning and testing dataset")
+        except:
+            pass
